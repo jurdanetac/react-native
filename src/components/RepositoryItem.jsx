91d80bbc083a5bcd0b5a6c4c71bd51bb315d4758
Juan Urdaneta <jurdanetac10@gmail.com>
2025-08-28 20:42:14 -0400

    Move forward with the card styles

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Image, Text, View, StyleSheet } from "react-native";
+import theme from '../theme'
 
 const RepositoryItem = ({ item }) => {
   const {
@@ -19,18 +20,47 @@ const RepositoryItem = ({ item }) => {
       width: 50,
       height: 50,
     },
+
+    pictureAndInfo: {
+      display: "flex",
+      flexDirection: "row",
+      // separation
+      gap: 30
+    },
+
+    // info = repo (author and name) + description + main language 
+    repoName: {
+      fontWeight: "bold",
+    },
+
+    language: {
+      padding: 10,
+      backgroundColor: theme.colors.primary,
+      maxWidth: "100%",
+      borderRadius: 100
+    },
+
+    // stars...
+    metadataContainer: {},
   });
 
   return (
     <View>
-      <Image source={{ uri: ownerAvatarUrl }} style={styles.image}></Image>
-      <Text>Full name: {fullName}</Text>
-      <Text>Description: {description}</Text>
-      <Text>Language: {language}</Text>
+      <View style={styles.pictureAndInfo}>
+        <Image source={{ uri: ownerAvatarUrl }} style={styles.image}></Image>
+        <View>
+          <Text style={styles.repoName}>{fullName}</Text>
+          <Text>{description}</Text>
+          <Text style={styles.language}>{language}</Text>
+        </View>
+      </View>
+
+      {/*
       <Text>Stars: {stargazersCount}</Text>
       <Text>Forks: {forksCount}</Text>
       <Text>Reviews: {reviewCount}</Text>
       <Text>Rating: {ratingAverage}</Text>
+     */}
     </View>
   );
 };
